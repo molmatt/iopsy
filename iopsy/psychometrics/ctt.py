35251177e3c6517f36e28ce5d5c0a889388b5547
molmatt <mol.matthew.j@gmail.com>
2021-05-31 19:57:55 -0700

    quick docs for alpha

--- iopsy/psychometrics/ctt.py
+++ iopsy/psychometrics/ctt.py
@@ -1,4 +1,13 @@
 def cronbachs_alpha(data):
+    """Cronbach's Alpha
+    
+    Cronbach's alpha is one of the most widely adopted estimates of a scale's inter-item reliability.
+    
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        df containing the item responses for the scale
+    """
     k = data.shape[1]
     sum_item_var = data.var().sum()
     scale_var = data.sum(axis = 1).var()
