f3b116a6af2aef08ac3a76ceab67e699eb9740e4
molmatt <mol.atthew.j@gmail.com>
2021-08-08 22:29:56 -0700

    dropping small groups

--- iopsy/analysis.py
+++ iopsy/analysis.py
@@ -1,4 +1,8 @@
 class Analysis:
+    """Analysis
+    
+    This is a generic base class for building more specific analyses from.
+    """
     def __init__(self, data, analysis, x, y = None, filters = None):
         self.data = data
         self.analysis = analysis
@@ -7,3 +11,13 @@ class Analysis:
         self.filters = filters
         self.p = None
         self.effect = None
+        
+def drop_small_n(data, x, n = None, prop = None):
+    vc = data[x].value_counts()
+    if n is not None:
+        keep = vc[vc > n].index
+    elif prop is not None:
+        props = vc/vc.sum()
+        keep = vc[props > prop].index
+    return data[data[x].isin(keep)]
+        
